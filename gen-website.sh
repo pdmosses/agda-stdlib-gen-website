1dd84a538790acc4b62827162c96a3ceee86ef50
Peter Mosses <18308236+pdmosses@users.noreply.github.com>
2026-03-25 18:40:25 +0100

    Create gen-website.sh

---
+++ gen-website.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Generate a website for a version of agda-stdlib
+
+# WARNING:
+# NOT YET TESTED FOR POTENTIAL FAILURES
+
+# Usage:
+# cd agda-stdlib-gen-website
+# sh gen-website.sh
+
+# Assumption:
+# The projects `agda-stdlib` and `agda-stdlib-gen-website` are siblings.
+
+cd ../agda-stdlib
+
+if [ -d .github/workflows ]; then
+  echo Disable CI workflows
+  mv .github/workflows .disabled;
+fi
+
+echo Copy fixed files
+
+cp -r ../agda-stdlib-gen-website/docs .
+cp ../agda-stdlib-gen-website/Makefile .
+cp ../agda-stdlib-gen-website/mkdocs.yml .
+cp ../agda-stdlib-gen-website/.gitignore .
+
+echo Update README.md
+
+cp ../agda-stdlib-gen-website/README-new.md .
+sd '\A(.|\n)*====*\n' '' README.md
+cat README.md >> README-new.md
+mv -f README-new.md README.md
+
+echo Generate Everything
+
+cp .github/tooling/* .
+cabal run GenerateEverything
+./index.sh
+
+echo Generate a website
+
+make -f Makefile check
+make -f Makefile clean-all
+make -f Makefile web
